1581550646301199c788e26473b08ea1a7e778ff
Torben Meyer <torben.meyer@bakdata.com>
2022-05-31 07:18:32 +0200

    Add missing description for core module

--- brute-force-core/build.gradle.kts
+++ brute-force-core/build.gradle.kts
@@ -1,3 +1,5 @@
+description = "Base module for Kafka plugins that deserialize messages of an unknown serialization format"
+
 dependencies {
     val kafkaVersion: String by project
     api(group = "org.apache.kafka", name = "kafka-clients", version = kafkaVersion)
