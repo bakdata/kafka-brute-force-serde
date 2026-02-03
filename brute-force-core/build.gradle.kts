bd73beede31a172d62dd7fa9b5e94f63e222fc79
Philipp Schirmer <philipp.schirmer@bakdata.com>
2026-02-03 12:13:46 +0100

    Migrate to version catalog

--- brute-force-core/build.gradle.kts
+++ brute-force-core/build.gradle.kts
@@ -1,10 +1,9 @@
 description = "Base module for Kafka plugins that deserialize messages of an unknown serialization format"
 
 dependencies {
-    val kafkaUtilsVersion: String by project
-    compileOnly(platform("com.bakdata.kafka:kafka-bom:$kafkaUtilsVersion"))
-    compileOnly(group = "org.apache.kafka", name = "kafka-clients")
+    compileOnly(platform(libs.kafka.bom))
+    compileOnly(libs.kafka.clients)
 
-    testImplementation(platform("com.bakdata.kafka:kafka-bom:$kafkaUtilsVersion"))
-    testImplementation(group = "org.apache.kafka", name = "kafka-clients")
+    testImplementation(platform(libs.kafka.bom))
+    testImplementation(libs.kafka.clients)
 }
